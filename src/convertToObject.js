d3fceca200f5a1421e523a6cd504336a8aebf121
Yurii Leniv <supusman32@gmail.com>
2026-03-02 15:44:57 +0200

    fdsfsd

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,23 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+
+  const m = sourceString.split(';');
+
+  for (let i = 0; i < m.length; i++) {
+    if (m[i].trim() === '') {
+      continue;
+    }
+
+    const parts = m[i].split(':');
+    const key = parts[0].trim(parts[0]);
+    const value = parts[1].trim(parts[1]);
+
+    result[key] = value;
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;
